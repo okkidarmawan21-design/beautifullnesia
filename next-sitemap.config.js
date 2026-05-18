5817290640fb0e9dad0b63f9148dada2f90bf844
okkidarmawan21-design <okkidarmawan21@gmail.com>
2026-05-18 20:58:41 +0700

    feat: sitemap untuk Google

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://beautifulnesia.vercel.app',
+  siteUrl: 'https://beautifullnesia.vercel.app',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
